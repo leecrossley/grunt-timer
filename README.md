bc100533caaa19548147aa0c0f20b680b614f058
Lee Crossley <leee@hotmail.co.uk>
2013-08-20 09:38:59 +0100

    Rough pass

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # grunt-timer
 
-grunt-timer.js times the execution of each of your grunt tasks automatically, calculates a total execution time and output to the console.
+grunt-timer.js times the execution of each of your grunt tasks automatically, calculates a total execution time and outputs to the console.
--- grunt-timer.js
+++ grunt-timer.js
@@ -1,5 +1,38 @@
 exports = module.exports = (function () {
-    var timer = {};
+    var timer = {}, grunt, hooker, start, last, task;
+
+    var gruntLog = function (text, head) {
+        if (head) {
+            grunt.log.header(head);
+        }
+        grunt.log.writeln(text);
+    };
+
+    timer.init = function (_grunt) {
+        grunt = _grunt;
+        hooker = _grunt.util.hooker;
+        start = new Date();
+        last = start;
+
+        var logCurrent = function () {
+            gruntLog(task + " " + new Date() - last], task + " timer");
+        };
+
+        hooker.hook(grunt.log, "timer", function () {
+            if (!task || task === grunt.task.current.nameArgs) {
+                return;
+            }
+            logCurrent();
+            task = grunt.task.current.nameArgs;
+            last = new Date();
+        });
+
+        process.on("exit", function () {
+            hooker.unhook(grunt.log, "timer");
+            // catch the last task
+            logCurrent();
+        });
+    }
 
     return timer;
 })();
